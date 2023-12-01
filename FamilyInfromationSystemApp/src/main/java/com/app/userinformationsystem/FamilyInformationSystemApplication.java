fc07cf63663037341df5b45757a7f693ef5d0ce0
Md. Abdullah Al Murad <17-34829-2@student.aiub.edu>
2023-12-02 01:38:24 +0600

    initial commit

---
+++ FamilyInfromationSystemApp/src/main/java/com/app/userinformationsystem/FamilyInformationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.app.userinformationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyInformationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyInformationSystemApplication.class, args);
+	}
+
+}
